5569f689cc864b0ce4fcbc38c4f8d4c79abf468c
Lucas Cadasqueves <lucas-rodrigo17@hotmail.com>
2024-05-03 14:08:08 -0300

    up teste
    teste

---
+++ web/config.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Autor: Thiago Ciavolela
+Data: 18/10/2023
+Descrição: Projeto OpenSource trabalhado emcima da EvolutionApi, para disparos em Massa.
+*/
+
+
+// Configurações EvolutionApi
+
+//KEY DO ARQUIVO ENV
+$apiKey = "APIKEY";
+
+//Url envio de Texto
+$url_api_txt = "https://seusite.com.br/message/sendText/SuaInstancia";
+
+//Url envio com imagem
+$url_api_image = "https://seusite.com.br/message/sendMedia/SuaInstancia";
+
+//Delay entre disparo de mensagem, tempo em segundos
+$delay = "10";
